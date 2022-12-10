8179af443e01deef61fef65eac3da61b2722c75d
Solemarc <62088534+Solemark@users.noreply.github.com>
2022-12-10 21:21:43 +1000

    Update characters.rs

--- genshin_impact_wiki/src/pages/characters.rs
+++ genshin_impact_wiki/src/pages/characters.rs
@@ -1,7 +1,43 @@
 use yew::prelude::*;
+use gloo_net::http::Request;
+use serde::Deserialize;
+
+#[derive(Clone, PartialEq, Deserialize)]
+struct Character{
+    name: String,
+    title: String,
+    vision: String,
+    weapon: String,
+    nation: String,
+    affiliation: String,
+    rarity: i8,
+    constellation: String,
+    birthday: String,
+    description: String
+}
+
 
 #[function_component(Characters)]
 pub fn characters() -> Html{
+
+    let characters: UseStateHandle<Vec<Character>> = use_state(|| vec![]);
+    {
+        let characters: UseStateHandle<Vec<Character>> = characters.clone();
+        use_effect_with_deps(move |_| {
+            let characters: UseStateHandle<Vec<Character>> = characters.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                let data: Vec<Character> = Request::get("https://api.genshin.dev/characters/all")
+                    .send()
+                    .await
+                    .unwrap()
+                    .json()
+                    .await
+                    .unwrap();
+                characters.set(data);
+            });
+            || ()
+        }, ());
+    }
     html!(
         <div>
             {"Character Page"}
